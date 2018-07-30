5969ddaeb18f3dcbcd312b37dae71321e760525b
Madeline Celis <madelinebcelis@gmail.com>
2018-07-30 00:51:09 +0000

    automatically backed up by learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -2,7 +2,7 @@ require 'pry'
 
 def nyc_pigeon_organizer(data)
 
-  data[:gender][:male].merge(data[:gender][:female])
+  data[:color][:gender][:male].merge(data[:gender][:female])
   binding.pry
 
 end
