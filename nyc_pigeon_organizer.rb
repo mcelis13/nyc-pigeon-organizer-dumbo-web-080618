87b6c083a5298579c3e155482bf90ff1f4649d47
Madeline Celis <madelinebcelis@gmail.com>
2018-07-30 01:00:36 +0000

    automatically backed up by learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -4,7 +4,7 @@ def nyc_pigeon_organizer(data)
   pigeon_list = {}
 
   data[:gender][:male].concat(data[:gender][:female]).each do |birdName|
-    pigeon_list => "#{birdName}" =>{}
+    pigeon_list = "#{birdName}" = {}
   end
 
   pigeon_list
