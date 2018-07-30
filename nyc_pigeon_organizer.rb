4f8ecf5082b91aa19c5d98e6d1db848286b774db
Madeline Celis <madelinebcelis@gmail.com>
2018-07-30 01:38:07 +0000

    automatically backed up by learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -17,7 +17,6 @@ def nyc_pigeon_organizer(data)
         nameArray.each do |name|
           if birdName == name
             pigeon_list[birdName][:color] << color.to_s
-            binding.pry
           end#end of if statement
         end#end of nameArray
       end#end of data[:color].each
