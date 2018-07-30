b724f8ebc46a2675b6cd6d976453232fa08d1fa3
Madeline Celis <madelinebcelis@gmail.com>
2018-07-30 01:18:43 +0000

    automatically backed up by learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -6,10 +6,11 @@ def nyc_pigeon_organizer(data)
   data[:gender][:male].concat(data[:gender][:female]).each do |birdName|
     if pigeon_list.has_key?("#{birdName}") == false
       pigeon_list[birdName] = {:color => [], :gender => [], :lives => []}
+      binding.pry
     end
   end
 
-  
+
 
   pigeon_list
 end
